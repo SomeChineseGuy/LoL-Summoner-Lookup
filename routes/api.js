e292e115cf759fc0c12af975dc721f2fe1008e7b
Alvin Ng <alvin.cl.ng@gmail.com>
2020-02-15 14:44:29 -0800

    Changed name to app

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const router = express.Router();
+const app = require('express');
+const router = app.Router();
 
 router.get('/', (req, res, next) => {
     res.send("One more update")
 })
+
+router.post('/search',req, res, next) => {
+    console.log(req.body)
+    res.send(
+        `Post request received! ${req.body}`
+        )
+})
+
 
 module.exports = router;
