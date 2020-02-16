b157d6160904e6b405632783b4f53a1af260a6b1
Alvin Ng <alvin.cl.ng@gmail.com>
2020-02-15 17:13:17 -0800

    Added env and tested out API call

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const axios = require('axios');
+require('dotenv').config()
 
 router.get('/', (req, res, next) => {
     res.send("One more update")
@@ -13,5 +15,18 @@ router.post('/search', (req, res, next) => {
     )
 })
 
+
+const riotApiTest = async () => {
+    let res = await axios
+    .get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Thatguy75?api_key=' + process.env.API_KEY)
+    
+    let data = res.data
+    console.log(data)
+}
+
+
+
+
+riotApiTest()
 
 module.exports = router;
